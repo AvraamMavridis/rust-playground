a6fa318de53d7576ac0efaf2dde60579cf5f9acf
AvraamMavridis <avraammauridis@gmail.com>
2017-11-19 17:06:51 +0100

    Feat: Generate random number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,11 +1,32 @@
+extern crate rand;
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please enter your guess");
+    let secret = rand::thread_rng().gen_range(1,101);
 
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to read line");;
+    loop {
+        println!("Please enter your guess");
 
-    println!("guess = {}", guess);
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        // Generate a random number between 1 and 100
+        println!("secret {}", secret);
+
+        match guess.cmp(&secret){
+            Ordering::Equal => {
+                println!("You won");
+                break;
+            },
+            Ordering::Greater => println!("Your number is greater"),
+            Ordering::Less => println!("Your number is smaller"),
+        }
+    }
 }
